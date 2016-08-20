4e37cbcaf3a74d92365032c02550fb973614200a
NikSaysIT@ya.ru <NikSaysIT@ya.ru>
2016-08-20 21:40:53 +0300

    First commit. The CORE of project. (Wlii be updated)

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="Cache-Control" content="no-store" />
+    <script src="jquery-3.1.0.min.js"></script>
+    <script src='scr.js'></script>
+    <title>GET|PHP_WILL_BE_HERE</title>
+  </head>
+  <body>
+    <header>
+      <nav></nav>
+    </header>
+    <main>
+      <nav></nav>
+      <div id="content">
+        <form method="post" id="FORM">
+          <p>text
+            <input type="text" name="text"/>
+          </p>
+        </form>
+        <button id='1'>SEND</button>
+        <div id="2"></div>
+      </div>
+    </main>
+
+  </body>
+</html>
