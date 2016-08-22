fb72743ffed4031bfe143fa2e963b917212bc3a7
NikSaysIT@ya.ru <NikSaysIT@ya.ru>
2016-08-22 12:01:36 +0300

    Added styles; Fixed permissiom problems; Fixed cashing of chat.html

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 jQuery(document).ready(function(){
+  // Disable caching of AJAX responses
+  $.ajaxSetup ({
+      cache: false
+    });
   //load chat.html to the chat field every 1 sec.
-  function get_it(){jQuery('#chatField').load('chat.html');};
-  setInterval(get_it, 1000);
+  function get_it(){
+    $.ajax({
+      url:"chat.html",
+      type:"GET",
+      ifModified:true,
+      success:function( data ) {
+        $("#chatMsg").replaceWith($("#chatMsg").html(data));
+        $("#chatField").scrollTop($("#chatField")[0].scrollHeight)
+      }
+    });
+  };
 
+
+  setInterval(get_it, 2500);
+  function load(){$("#chatField").scrollTop($("#chatField")[0].scrollHeight);};
   //Asking for nickname
-  var name=prompt('Nickname');
-  while(name==null||name==''){var name=prompt('Please write your nickname, sir.');};
-
+  var name=prompt("Nickname");
+  while(name==null||name==""){var name=prompt("Please write your nickname, sir.");};
   $("#sendButton").click(function(){
     //Configuring what data is coming to server and sending it by POST
-    var data='name=' + name + '&' + $('#textField').serialize();
-    alert(data);
+    var data="name=" + name + "&" + $("#textField").serialize();
     $.ajax({
-      url:'server.php',
+      url:"server.php",
       type:"POST",
       data:data
     });
--- server.php
+++ server.php
@@ -1,18 +1,18 @@
 <?php
   //Configuring text inserted in chat.html as <boldNick:msgText.newline>
-  $TEXT='<b>'.$_POST['name'].'</b>:  '.$_POST['text']. '<br>' ;
+  $TEXT="<b>".$_POST['name']."</b>:  ".$_POST['text']. "</div><br>" ;
   switch ($_POST['text']) {
     //If msgText is emptu reject it
-    case '':
+    case "":
       break;
     //That command allows you to clear chat
     //Without this meta tag chat.html caches and don't updates
-    case '/cls':
-      file_put_contents('chat.html', '<meta http-equiv="Cache-Control" content="no-store" />');
+    case "/cls":
+      file_put_contents("chat.html", "<meta http-equiv='Cache-Control' content='no-store' />");
       break;
     //File_append is like >>. It writes text to the end of file instead of rewriting whole file
     default:
-      file_put_contents('chat.html', $TEXT, FILE_APPEND);
+      file_put_contents("chat.html", $TEXT, FILE_APPEND);
       break;
   }
 ?>
